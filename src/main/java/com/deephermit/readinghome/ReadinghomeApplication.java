0facc443717abf20690aeaacaa3d205a2fc3061e
DeepHermit <343506776@qq.com>
2019-05-20 14:44:04 +0800

    初始建立项目，一个新建项目

---
+++ src/main/java/com/deephermit/readinghome/ReadinghomeApplication.java
@@ -0,0 +1,13 @@
+package com.deephermit.readinghome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadinghomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadinghomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deephermit/readinghome/ReadinghomeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.deephermit.readinghome;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReadinghomeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
